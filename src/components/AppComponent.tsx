110a4989dd0019f1ecaca9a9e17760fc36ad5533
Pat <podolski.patryk@gmail.com>
2019-05-25 17:22:07 +0200

    Fe gui folders (#14)
    * first reorganisation of components folders
    
    * refactoring folder structure

--- src/components/AppComponent.tsx
+++ src/components/AppComponent.tsx
@@ -1,48 +1,17 @@
 import * as React from 'react';
 import {
-  Route,
   Switch,
   Router,
 } from 'react-router';
-import User from 'src/components/testComponent';
 import history from 'src/configuration/history';
 // HELPERS
-import {
-  pipe,
-  createLinksConfiguration,
-  createArrayOfJSXLinks,
-} from './appComponent.helpers';
-// MATERIAL UI
-import Toolbar from '@material-ui/core/Toolbar';
-// import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import AssignmentInd from '@material-ui/icons/AssignmentInd';
-import Fingerprint from '@material-ui/icons/Fingerprint';
-import Email from '@material-ui/icons/Email';
-import Android from '@material-ui/icons/Android';
-import LocationOn from '@material-ui/icons/LocationOn';
-import Whatshot from '@material-ui/icons/Whatshot';
+import { createLinksConfiguration, createRouteComponents } from './appComponent.helpers';
 // STYLES
-import { appComponentStyle as cls } from './appComponent.style';
 import './appComponent.style';
-
-const HomePage: React.SFC = (props: any) => {
-  return (
-    <div>
-      HomePage
-      </div>
-  );
-};
-
-const List: React.SFC = (props: any) => {
-  return (
-    <div>
-      List
-    </div>
-  );
-};
+// Header
+import Header from './header/header';
+// Footer
+import Footer from './footer/footer';
 
 class App extends React.Component<any, any> {
   public render() {
@@ -51,107 +20,17 @@ class App extends React.Component<any, any> {
         history={history}
       >
         <div>
-
           <div className="App">
-            <header className={cls.appHeader}>
-              <Toolbar className={cls.toolbarMain}>
-                <Grid
-                  container={true}
-                  xs={12}
-                  item={true}
-                  alignItems={'center'}
-                  justify={'center'}
-                  direction={'row'}
-                >
-                  <Grid xs={2} item={true} />
-                  <Grid xs={8} item={true} >
-                    <Typography
-                      component="h2"
-                      variant="h5"
-                      color="inherit"
-                      align="center"
-                      noWrap={true}
-                      className={cls.toolbarTitle}
-                    >
-                      React-Redux Boilerplate
-                  </Typography>
-                  </Grid>
-                  <Grid
-                    xs={1}
-                    item={true}
-                    className={cls.headerBtnGrid('right')}
-                  >
-                    <Button
-                      // variant="outlined"
-                      size="small"
-                      className={cls.headerBtn}
-                    >
-                      <AssignmentInd />
-                      <div
-                        className={cls.headerBtnText}
-                      >
-                        Sign In
-                      </div>
-                    </Button>
-                  </Grid>
-                  <Grid
-                    xs={1}
-                    item={true}
-                    className={cls.headerBtnGrid('left')}
-                  >
-                    <Button
-                      // variant="outlined"
-                      size="small"
-                      className={cls.headerBtn}
-                    >
-                      <Fingerprint />
-                      <div
-                        className={cls.headerBtnText}
-                      >
-                        Sign Up
-                      </div>
-                    </Button>
-                  </Grid>
-                </Grid>
-              </Toolbar>
-              <nav>
-                <Toolbar
-                  // variant="dense"
-                  className={cls.tootlbarSecondary}
-                >
-                  <Typography
-                    color="inherit"
-                    noWrap={false}
-                    align="center"
-                    className={cls.toolbarLinks}
-                  >
-                    {
-                      pipe(
-                        createLinksConfiguration,
-                        createArrayOfJSXLinks
-                      )()
-                    }
-                  </Typography>
-                </Toolbar>
-              </nav>
-            </header>
+            <Header />
             <section>
               <Switch>
-                <Route exact={true} path="/" component={HomePage} />
-                <Route exact={true} path="/user" component={User} />
-                <Route exact={true} path="/list" component={List} />
+                {
+                  createLinksConfiguration()
+                    .map(createRouteComponents)
+                }
               </Switch>
             </section>
-            <footer className={cls.appFooter}>
-              <Toolbar
-                className={cls.toolbarFooter}
-              >
-                <Email />
-                <Android />
-                <LocationOn />
-                <Whatshot />
-              </Toolbar>
-            </footer>
+            <Footer />
           </div>
         </div>
       </Router>
--- src/components/appComponent.helpers.tsx
+++ src/components/appComponent.helpers.tsx
@@ -1,44 +1,38 @@
 import * as React from 'react';
-import { NavLink, NavLinkProps } from 'react-router-dom';
-import Button from '@material-ui/core/Button';
-import { appComponentStyle as st } from './appComponent.style';
+import { NavLinkProps } from 'react-router-dom';
+import { Route } from 'react-router';
+import HomePage from 'src/components/sections/homePage/homePage';
+import WikiSearch from 'src/components/sections/wikiSearch/wikiSearch';
+import User from 'src/components/sections/user/user';
 
-class NavLinkContent implements NavLinkProps {
+export class NavLinkContent implements NavLinkProps {
   public link: string;
   public to: string;
+  public component: React.ComponentType;
 }
 
 export const createLinksConfiguration = (): NavLinkContent[] => ([
   {
     to: '/',
     link: 'Home',
+    component: HomePage,
   },
   {
     to: '/wiki',
     link: 'Wiki-Search',
+    component: WikiSearch,
   },
   {
     to: '/user',
     link: 'User Details',
+    component: User,
   },
 ]);
 
-export const createJSXLink = (props: NavLinkContent, index: number): JSX.Element => (
-  <NavLink
-    key={index}
-    to={props.to}
-    className={st.navLink}
-    {...props} >
-    <Button>
-      {props.link}
-    </Button>
-  </NavLink>
+export const createRouteComponents = (linkObject: NavLinkContent) => (
+  <Route
+    exact={true}
+    path={linkObject.to}
+    component={linkObject.component}
+  />
 );
-
-export const createArrayOfJSXLinks = (
-  linksConfig: NavLinkContent[]
-): JSX.Element[] => linksConfig.map(createJSXLink);
-
-export const _pipeFn = (fn1: any, fn2: any) => fn2(fn1());
-
-export const pipe = (...arrayOfFn: any) => () => arrayOfFn.reduce(_pipeFn);
