533052d8d4cdf572cb0ec8357034e12125cb490c
Patryk Podolski <podolski.patryk@gmail.com>
2019-06-09 18:21:12 +0200

    center input suggestions and limit width

--- src/components/sections/wikiSearch/wikiSearch.tsx
+++ src/components/sections/wikiSearch/wikiSearch.tsx
@@ -2,10 +2,22 @@ import * as React from 'react';
 import { connect } from 'react-redux';
 import Button from '@material-ui/core/Button';
 import IntegrationAutosuggest from './wikiSuggest/wikiSuggest';
+import { style } from 'typestyle';
+
+const cl = {
+  sectionStyle: style({
+    maxWidth: '300px',
+    display: 'inline-block',
+  }),
+};
+
+
 export class WikiSearch extends React.Component {
   public render() {
     return (
-      <section>
+      <section
+        className={cl.sectionStyle}
+      >
         <input type="textfield" />
         <IntegrationAutosuggest />
         <Button> Submit </Button>
